70a5f1d74fd19ae95fa585ce1c917d35fcc28555
Panji Arlin Saputra <75957797+panjiarlins@users.noreply.github.com>
2023-08-10 11:02:48 +0700

    test-deploy

---
+++ src/lib/firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+const {
+  REACT_APP_FIREBASE_apiKey,
+  REACT_APP_FIREBASE_authDomain,
+  REACT_APP_FIREBASE_projectId,
+  REACT_APP_FIREBASE_storageBucket,
+  REACT_APP_FIREBASE_messagingSenderId,
+  REACT_APP_FIREBASE_appId,
+} = process.env;
+
+const firebaseConfig = {
+  apiKey: REACT_APP_FIREBASE_apiKey,
+  authDomain: REACT_APP_FIREBASE_authDomain,
+  projectId: REACT_APP_FIREBASE_projectId,
+  storageBucket: REACT_APP_FIREBASE_storageBucket,
+  messagingSenderId: REACT_APP_FIREBASE_messagingSenderId,
+  appId: REACT_APP_FIREBASE_appId,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
